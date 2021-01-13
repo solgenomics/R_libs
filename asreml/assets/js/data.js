7320d2f190b3ab466629d7e5651248b73f5c2936
root <nmorales3142@gmail.com>
2021-01-13 14:22:26 +0000

    asreml

---
+++ asreml/assets/js/data.js
@@ -0,0 +1,8 @@
+data = {
+    colorScheme: {
+        primary: "mdl-color--light-blue-700",
+        secondary: "mdl-color--light-blue-A100"
+    },
+    instructions: "<p>As we have been unable to directly activate your ASReml-R installation we have validated your activation code through your browser.<br><b>Please click COPY then run asreml.license.activate() again within your R session and paste the replacement code when prompted to enter an activation code.</b></p>",
+    errors: {}
+}
